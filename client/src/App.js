1474250ecae40946f169950e7c3712c92ceb6f6e
Ved Patel <84237281+vedXpatel@users.noreply.github.com>
2023-04-26 08:30:27 +0000

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./components/Login";
 import Form from "./components/Form";
 import Dashboard from "./components/Dashboard.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
   return (
     <>
       <Router >
           <Routes>
-            <Route path="/" element={<Login/>} />
+            <Route path="/" element={<Home/>} />
+            <Route path='/login' element={<Login/>} />
             <Route path="/form" element={<Form/>} />
             <Route path="/dashboard" element={<Dashboard/>} />
           </Routes>
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import home1 from "../assets/home1.png";
+import home2 from "../assets/home2.png";
+import home3 from "../assets/home3.png";
+import home4 from "../assets/home4.png";
+import home5 from "../assets/home5.png";
+import "./landing.css";
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <input type="checkbox" id="active" />
+            <label for="active" class="menu-btn"><span></span></label>
+            <label for="active" class="close"></label>
+            <div class="wrapper">
+                <ul>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">Feedback</a></li>
+                </ul>
+            </div>
+            <div className="container1">
+                <img src={home1} alt="" className="home1" />
+            </div>
+            <div className="container2" style={{ paddingTop: "20vh" }}>
+                <img src={home2} alt="" className="home2" />
+            </div>
+            <div className="container3">
+                <img src={home3} alt="" className="home3" />
+            </div>
+            <div className="container4">
+                <img src={home4} alt="" className="home4" />
+            </div>
+            <div className="container5">
+                <img src={home5} alt="" className="home5" />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
