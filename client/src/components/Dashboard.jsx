63b841514ba18bb6c784ff1c2e82106a6f876ff3
Ved Patel <84237281+vedXpatel@users.noreply.github.com>
2023-04-28 07:57:17 +0000

    risk calculation

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -25,34 +25,124 @@ const Dashboard = () => {
         setEmail(String(tempEmail));
         // const uid = user.uid;
         // ...
+        const fetchData = async () => {
+          try {
+            const response = await axios.get("https://script.google.com/macros/s/AKfycbwNDH8iTJl6LT-bEp16QMWfADiGqSWpH9ZbEpuL76IswiafUQ424BC5Jtjk5134E7K_bw/exec");
+            setFormData(response.data.data);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        fetchData();
       } else {
         alert(`Login to get personalized data`);
         navigate("/login")
       }
     });
 
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://script.google.com/macros/s/AKfycbwNDH8iTJl6LT-bEp16QMWfADiGqSWpH9ZbEpuL76IswiafUQ424BC5Jtjk5134E7K_bw/exec");
-        setFormData(response.data.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
+    // const fetchData = async () => {
+    //   try {
+    //     const response = await axios.get("https://script.google.com/macros/s/AKfycbwNDH8iTJl6LT-bEp16QMWfADiGqSWpH9ZbEpuL76IswiafUQ424BC5Jtjk5134E7K_bw/exec");
+    //     setFormData(response.data.data);
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
+    // fetchData();
   },[])
   
-  // console.log(formData);
+  console.log(formData);
 
-  let mainIndex;
+  let mainIndex,userFormData;
   const searchEmail = () => {
       for (let i = 0; i < formData.length; i++) {
           if(formData[i][8] === email){
             mainIndex = i;
+            userFormData = formData[mainIndex]
           }
       }
     }
-  if(formData != undefined) {searchEmail()};
+  if(formData != undefined) {searchEmail();};
+
+  function calculateRiskScore() {
+
+    // Assign risk values to questions
+    const riskValues = {
+      secureIncome: {
+        "Very Stable": 10,
+        "Stable": 15,
+        "Somewhat Stable": 20,
+        "Unstable" : 25,
+        "Very Unstable" : 30
+      },
+      financiallyDependent: {
+        0: 10,
+        1: 20,
+        2: 30
+      },
+      emiAllocation: {
+        "None": 10,
+        "Less than 10" : 15,
+        "10 -20" : 20,
+        "20 -30" : 25,
+        "Above 30" : 30,
+        // lessThan25Percent: 10,
+        // between25And50Percent: 20,
+        // moreThan50Percent: 30
+      },
+      stockInvestmentPreference: {
+        "Buy more of the investment" : 30,
+        "Hold onto the investment and sell nothing" : 20,
+        "Sell a portion of the remaining investment" : 15,
+        "Sell all of the remaining investment" : 10,
+        // sellAll: 10,
+        // holdAndMonitor: 20,
+        // buyMore: 30
+      },
+      investmentHorizon: {
+        shortTerm: 10,
+        mediumTerm: 20,
+        longTerm: 30
+      },
+      riskAppetite: {
+        "Strongly Agree" : 30,
+        "Agree" : 25,
+        "Neutral" : 20,
+        "Disagree" : 15,
+        "Strongly Disagree" : 10
+      },
+      portfolioAllocation: {
+        "Savings account and fixed deposits" : 10,
+        "Bonds" : 15,
+        "Equities or Mutual Funds" : 25,
+        "Real Estate" : 15,
+      },
+      investmentAmount: {
+        lessThan10k: 10,
+        between10kAnd50k: 20,
+        moreThan50k: 30
+      }
+    };
+  
+    // Calculate risk score
+    const riskScore = riskValues.secureIncome[secureIncome]
+      + riskValues.financiallyDependent[financiallyDependent]
+      + riskValues.emiAllocation[emiAllocation]
+      + riskValues.stockInvestmentPreference[stockInvestmentPreference]
+      + riskValues.investmentHorizon[investmentHorizon]
+      + riskValues.riskAppetite[riskAppetite]
+      + riskValues.portfolioAllocation[portfolioAllocation]
+  
+    // Return risk score
+    return riskScore;
+  }
+  
+  // Call the function to calculate the risk score
+  const riskScore = calculateRiskScore();
+  
+  // Log the risk score to the console
+  console.log("Your risk score is: " + riskScore);
+  
   
 
   return (
